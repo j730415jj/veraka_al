60ba5ea09449b7d71c12517e5affe1b7e57ab8c3
j730415jj <j730415@gmail.com>
2026-02-04 21:20:49 +0900

    tailwind 설정에 components 경로 추가

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",       // 혹시 몰라 남겨둠
+    "./components/**/*.{js,ts,jsx,tsx}", // 👈 [중요] 여기가 진짜 파일들이 있는 곳입니다!
+    "./*.{js,ts,jsx,tsx}"                // App.tsx, main.tsx 같은 바깥 파일들
   ],
   theme: {
     extend: {},
